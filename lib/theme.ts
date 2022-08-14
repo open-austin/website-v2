8382adb9bc0394fff80137b5a39c82482fb91833
newswim <ofenixculpa@gmail.com>
2022-08-14 10:38:58 -0500

    add `theme` and basic overrides

---
+++ lib/theme.ts
@@ -0,0 +1,57 @@
+import {
+  extendTheme,
+  type ThemeConfig,
+  // type Theme, // Uncomment to see the Theme API
+  // theme as baseTheme,
+} from '@chakra-ui/react'
+
+// baseTheme.colors.messenger[300]
+
+const config: ThemeConfig = {
+  initialColorMode: 'system',
+  useSystemColorMode: true,
+  // disableTransitionOnChange: true,
+}
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const components = {
+  Link: {
+    baseStyle: {
+      color: colors.brand['700'],
+    },
+  },
+  SocialButton: {
+    baseStyle: {
+      color: '#e77131',
+    },
+  },
+}
+
+// Note: there are conflicting examples of how to use `extendTheme`, and
+// its docs say that the API is unwiedly. Also, it's really poorly typed.
+// See: https://chakra-ui.com/docs/styled-system/color-mode#updating-the-theme-config
+// See: https://chakra-ui.com/getting-started/nextjs-guide#customizing-theme
+export const theme = extendTheme({
+  config,
+  colors,
+  components,
+  // textStyles: {
+  //   link: {
+  //     color: '#2a69ac',
+  //   },
+  // h1: {
+  //   // you can also use responsive styles
+  //   fontSize: ['48px', '72px'],
+  //   fontWeight: 'bold',
+  //   lineHeight: '110%',
+  //   letterSpacing: '-2%',
+  // },
+  // },
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,15 @@
+// Setup for Chakra and Next environments
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import { extendTheme } from '@chakra-ui/react'
-import { ChakraProvider } from '@chakra-ui/react'
-
-// Theme
-
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-const theme = extendTheme({ colors })
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { theme } from '../lib/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
