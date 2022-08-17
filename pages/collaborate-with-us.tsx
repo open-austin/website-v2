f575655445c4aadfa067401a0988415bcafea110
attali-david <dattali93@gmail.com>
2022-08-17 11:47:23 -0500

    Added google form and basic styling to Collaborate component. Added Header component to collaborate page.

--- pages/collaborate-with-us.tsx
+++ pages/collaborate-with-us.tsx
@@ -1,8 +1,10 @@
 import Collaborate from "../components/Collaborate";
+import Header from "../components/header";
 
 const CollaborateWithUs = () => {
   return (
     <>
+      <Header />
       <Collaborate />
     </>
   );
