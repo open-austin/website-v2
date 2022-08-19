a160cda196180a5504cf2cc4e4ef3c5c7e2228e8
newswim <ofenixculpa@gmail.com>
2022-08-19 17:36:44 -0500

    leave helpful comment

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,3 +1,16 @@
+/**
+ * This file is just an example of how we might override the theme.
+ * Nothing here is really doing much for us, with the exception of
+ * `Link`, which is overriden just so we could see links more easily.
+ *
+ * As I was learning about Chakra and it's design system assuptions,
+ * I left some links so that the next person will have a place to
+ * get started. Good luck!
+ *
+ * @Updated (08/19/2022) @newswim
+ *
+ */
+
 import {
   extendTheme,
   type ThemeConfig,
@@ -35,10 +48,13 @@ const components = {
   },
 }
 
-// Note: there are conflicting examples of how to use `extendTheme`, and
-// its docs say that the API is unwiedly. Also, it's really poorly typed.
-// See: https://chakra-ui.com/docs/styled-system/color-mode#updating-the-theme-config
-// See: https://chakra-ui.com/getting-started/nextjs-guide#customizing-theme
+/**
+ * Note: there are conflicting examples of how to use `extendTheme`, and
+ * its docs say that the API is unwiedly. Also, it's really poorly typed.
+ *
+ * @see https://chakra-ui.com/docs/styled-system/color-mode#updating-the-theme-config
+ * @see https://chakra-ui.com/getting-started/nextjs-guide#customizing-theme
+ */
 export const theme = extendTheme({
   config,
   colors,
