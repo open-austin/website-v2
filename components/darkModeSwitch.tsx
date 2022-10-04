01735c54802ec8ecbe3ca1aaa23fd6d12d7dd0dd
newswim <ofenixculpa@gmail.com>
2022-10-04 00:01:38 -0500

    tweak darkMode switch

--- components/darkModeSwitch.tsx
+++ components/darkModeSwitch.tsx
@@ -1,18 +1,17 @@
-import { Button, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { IconButton, useColorMode } from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
 export default function DarkModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode()
-  const bg = useColorModeValue('gray.500', 'gray.800')
-  const hover = useColorModeValue('gray.600', 'gray.700')
 
   return (
-    <Button
-      size={'md'}
+    <IconButton
+      aria-label="Toggle dark mode"
       onClick={toggleColorMode}
-      bg={bg}
-      _hover={{ bg: hover }}
-    >
-      {colorMode === 'light' ? '🌑 Dark Mode' : '💡 Light Mode'}
-    </Button>
+      bg="transparent"
+      size="lg"
+      color={colorMode === 'light' ? 'blue.700' : 'yellow.300'}
+      icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+    />
   )
 }
